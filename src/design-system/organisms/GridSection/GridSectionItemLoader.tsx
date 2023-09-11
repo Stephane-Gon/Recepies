cd4324d544746e1577b758a32aa2e7ff2d9d5dfa
Stephane-Gon <stephane_work@outlook.com>
2023-09-11 19:34:28 +0100

    added loading state to GridSection organism

---
+++ src/design-system/organisms/GridSection/GridSectionItemLoader.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css'
+
+
+const GridSectionItemLoader = () => {
+
+  return (
+    <div className={`${styles.itemLoader} skeleton`}>
+      
+    </div>
+  )
+}
+
+export default GridSectionItemLoader
--- src/design-system/organisms/GridSection/index.tsx
+++ src/design-system/organisms/GridSection/index.tsx
@@ -2,10 +2,12 @@ import GridSectionRoot from "./GridSectionRoot"
 import GridSectionTitle from "./GridSectionTitle"
 import GridSectionGrid from "./GridSectionGrid"
 import GridSectionItem from "./GridSectionItem"
+import GridSectionItemLoader from "./GridSectionItemLoader"
 
 export const GridSection = {
   Title: GridSectionTitle,
   Root: GridSectionRoot,
   Grid: GridSectionGrid,
-  Item: GridSectionItem
+  Item: GridSectionItem,
+  ItemLoader: GridSectionItemLoader
 }
