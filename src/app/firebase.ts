a179b13bdf6e6d667fa7bc309e724a960810c0af
Stephane-Gon <stephane_work@outlook.com>
2023-09-19 20:44:18 +0100

    removed firebase packages and logic

--- src/app/firebase.ts
+++
@@ -1,7 +0,0 @@
-import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
-import app from '../firebase/config'
-
-const db = getFirestore(app)
-const auth = getAuth()
-export { db, auth }
